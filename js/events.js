2163780e1f44544b0f154a397081c0f071849d5a
dsanalog <dsanalog@gmail.com>
2019-01-11 17:22:20 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,8 +8,8 @@
   }
   
   function pressIt(){
-    $('input#typing').on('keyup',function(event){
-    if (event.which === 70){ pressIt(); }
+    $('input#typing').on('keyup',function(e){
+    if (e.which === 70){ pressIt(); }
   });
   }
   
