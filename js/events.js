6ced928bfcab37fc373384be0ff9477178b58fb1
dsanalog <dsanalog@gmail.com>
2019-01-11 16:29:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,12 +11,12 @@
     key = event.which;
     trigger = 71;
     if(key === trigger){
-      alert("Did you mean to press G?")
+      alert("Did you mean to press G?");
     }
   }
   
   function submitIt(){
-    
+    alert("Perpare to submit!");
   }
 $(document).ready(function(){
 
@@ -24,5 +24,6 @@ $(document).ready(function(){
   $('p').on('click',getIt);//.on same as .click
   $('img').on('load', frameIt);
   $('input#typing').on('keydown',pressIt);
+  $('form').on('submit',submitIt);
   
 });
