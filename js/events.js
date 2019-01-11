4025e9b7053a759f33310c4ebd3dc8f4917e5f3a
dsanalog <dsanalog@gmail.com>
2019-01-11 16:02:05 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
 //define functions here
-
+  function getIt(){
+    
+  }
+  
+  function frameIt(){
+    
+  }
+  
+  function pressIt(){
+    
+  }
+  
+  function submitIt(){
+    
+  }
 $(document).ready(function(){
 
-// call functions here
+  // call functions here
 
+  
+  
 });
